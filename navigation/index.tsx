6333bd63e30202a353067ca115df4df729f2a439
Gyuri21 <gyuri21hegedus@gmail.com>
2022-01-10 11:33:09 +0100

    kis stilus modalban

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -66,7 +66,7 @@ function BottomTabNavigator() {
         name="TabOne"
         component={TabOneScreen}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
+          title: 'Dolgozók',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Pressable
--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -7,9 +7,11 @@ import { Text, View } from '../components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
+      <Text style={styles.title}>Névjegy</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(0,0,0,1)" />
+      <Text style={styles.text}>Gyakorló program</Text>
+      <Text style={styles.text}>Hegedűs György</Text>
+      <Text style={styles.text}>2022.01.10</Text>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
@@ -22,14 +24,22 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
+    color: 'black'
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: 'black'
   },
   separator: {
     marginVertical: 30,
-    height: 1,
+    height: 2,
     width: '80%',
   },
+  text:{
+    color: 'black',
+    fontSize: 15,
+    fontWeight: 'bold'
+  }
 });
